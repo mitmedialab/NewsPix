3d93d616f7647a9cd00e168c329ecf85d3742c63
jayvachon <jaylvachon@gmail.com>
2014-12-08 12:52:00 -0500

    add new extension

---
+++ myFlask/extensions/js/newspix.js
@@ -0,0 +1,22 @@
+$.getJSON('json/stories.json', function (data) {
+
+})
+.done(function (data) {
+	var stories = [];
+	$.each(data, function(key, val) {
+		stories.push(val);
+	});
+
+	var randInt = Math.floor(Math.random() * stories[0].length);
+	var randStory = stories[0][randInt.toString()];
+
+	var bg = randStory['image'];
+	$('body').css('background-image', bg);
+	var path = '../images/news/';
+	$('body').css('background-image', 'url("' + path+bg + '")');
+
+	var headlineLink = '<a href="' + randStory['url'] + '">';
+	var headlineContent = headlineLink + randStory['headline'] + '</a>';
+
+	$(headlineContent).appendTo('.headline');
+});
---
+++ myFlask/extensions/legacy/README.md
@@ -0,0 +1,1 @@
+Could probably be deleted altogether
