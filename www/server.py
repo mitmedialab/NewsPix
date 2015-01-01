a28eaed8aab564cb023302ed067dc7e988e4b9fb
kanarinka <kanarinka@gmail.com>
2015-01-01 08:19:56 -0500

    re-org for web-based assets
    move about page to server

---
+++ www/server.py
@@ -0,0 +1,56 @@
+import os
+import ConfigParser
+import requests
+from bson.json_util import dumps
+from flask import Flask,render_template,request
+from pymongo import MongoClient 
+
+app = Flask(__name__)
+app.debug = True
+
+
+# constants
+CONFIG_FILENAME = 'app.config'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# read in app config
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(BASE_DIR,CONFIG_FILENAME))
+
+# MongoDB & links to each collection
+'''uri = "mongodb://"+ config.get('db','user')+ ":"+ config.get('db','pass')+"@" +config.get('db','host') + ":" + config.get('db','port')+"/?authSource="+config.get('db','auth_db')
+print uri
+db_client = MongoClient(uri)
+app.db = db_client[config.get('db','name')]
+app.db_weather_collection = app.db[config.get('db','weather_collection')]
+app.db_jokes_collection = app.db[config.get('db','jokes_collection')]
+'''
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
