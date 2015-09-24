f745c6bb2203e565200bceaa8adf7cef36dbbf94
kanarinka <kanarinka@gmail.com>
2015-09-24 15:16:00 -0400

    just changed server URL

--- extension/extension.config.js
+++ extension/extension.config.js
@@ -1,3 +1,6 @@
 // set this in newspix.js as well
-//var SERVER_URL = "http://127.0.0.1:5000"; 
-var SERVER_URL = "http://dev.newspix.today";
+
+//var SERVER_URL = "http://dev.newspix.today";
+
+var SERVER_URL = "http://127.0.0.1:5000"; 
+ //var SERVER_URL = "http://dev.newspix.today";
