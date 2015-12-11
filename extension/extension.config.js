7ccdd0667d8cba638c35c5bc87a911ebecb93e31
Kapaya <kkatongo@mit.edu>
2015-12-10 22:17:20 -0500

    added firefox extension

--- extension/extension.config.js
+++ extension/extension.config.js
@@ -1,5 +1,5 @@
-var SERVER_URL = "http://127.0.0.1:5000";
-//var SERVER_URL = "http://dev.newspix.today";
+//var SERVER_URL = "http://127.0.0.1:5000";
+var SERVER_URL = "http://dev.newspix.today";
 
 var twitter_share_url = "https://twitter.com/intent/tweet?text=";
 var facebook_share_url = "https://www.facebook.com/sharer/sharer.php?u=";
